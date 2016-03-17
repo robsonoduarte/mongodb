3dbe963b1b3614d88a86e477e13c9f7466e4c900
Robson Duarte <robson.o.d@gmail.com>
2016-03-17 09:45:16 -0300

    simple example of spark java.

--- mongo-university-m101j/src/main/java/br/com/mystudies/mongo_university_m101j/App.java
+++ mongo-university-m101j/src/main/java/br/com/mystudies/mongo_university_m101j/App.java
@@ -1,5 +1,11 @@
 package br.com.mystudies.mongo_university_m101j;
 
+import static spark.Spark.get;
+
 public class App {
+
+	public static void main(String[] args) {
+		get("hello", (req, res) -> "Hello World");
+	}
 
 }
