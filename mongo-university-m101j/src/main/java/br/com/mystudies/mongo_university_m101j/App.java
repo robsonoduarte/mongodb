82670ad79fa3d200e3dc665623898885507fbeac
Robson Duarte <robson.o.d@gmail.com>
2016-03-17 09:34:40 -0300

    initial commit of project

---
+++ mongo-university-m101j/src/main/java/br/com/mystudies/mongo_university_m101j/App.java
@@ -0,0 +1,5 @@
+package br.com.mystudies.mongo_university_m101j;
+
+public class App {
+
+}
---
+++ mongo-university-m101j/src/test/java/br/com/mystudies/mongo_university_m101j/AppTest.java
@@ -0,0 +1,12 @@
+package br.com.mystudies.mongo_university_m101j;
+
+import org.junit.Test;
+
+public class AppTest {
+
+
+	@Test
+	public void test() {
+	}
+
+}
