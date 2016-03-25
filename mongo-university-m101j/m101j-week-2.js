6020d3c1d9764a3882b47e56a12ef70f7ce953ef
Robson Duarte <robson.o.d@gmail.com>
2016-03-25 09:33:28 -0300

    add js file for lectures of week 2

---
+++ mongo-university-m101j/m101j-week-2.js
@@ -0,0 +1,51 @@
+//db.test.people.find({c: 'Robson Duarte'},{a : true, _id : false})
+
+/*
+for(i = 0; i < 1000; i++){    
+ names = ['exam', 'essay', 'quiz'];
+    for(j=0; j < 3; j++){
+            var student = {'student' : i, 'type' : names[j], score: Math.round(Math.random() * 100 )}
+            db.scores.insert(student);
+    }       
+}    
+*/
+
+//db.scores.find({type: 'essay', score: 50}, {student: true, _id : false})
+//db.scores.find({score : { $gt : 95 , $lte : 98 }, type : 'essay' } )
+
+/*
+db.people.insert({name : 'Robson'})
+db.people.insert({name : 'Ana'})
+db.people.insert({name : 'Cibele'})
+db.people.insert({name : 'Antonio'})
+db.people.insert({name : 'Aparecida'})
+db.people.insert({name : 'Maria'})
+db.people.insert({name : 'João'})
+*/
+
+/*
+db.people.insert({name : 'João', profession: 'pescador'})
+db.people.insert({name : 'Pedro', profession: 'Motorista'})
+db.people.insert({name : 'China', age: 30 ,  profession: 'Motorista'})
+*/
+
+
+
+db.people.find()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
