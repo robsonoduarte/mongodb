3e8fcfd68ed17d152f8f431553e2243c33ef7543
Robson Duarte <robson.o.d@gmail.com>
2016-03-26 10:06:25 -0300

    add examples of cursors ( Lecture Querying, Cursors )

--- mongo-university-m101j/m101j-week-2.js
+++ mongo-university-m101j/m101j-week-2.js
@@ -9,11 +9,11 @@ for(i = 0; i < 1000; i++){
             db.scores.insert(student);
     }       
 } 
-
-
+
+
 // examples of $gt, $lt and using field selection 
-db.scores.find({type: 'essay', score: 50}, {student: true, _id : false})
-db.scores.find({score : { $gt : 95 , $lte : 98 }, type : 'essay' } )
+db.scores.find({type: 'essay', score: 50}, {student: true, _id : false})
+db.scores.find({score : { $gt : 95 , $lte : 98 }, type : 'essay' } )
 
 
 
@@ -26,35 +26,35 @@ db.people.insert({name : 'Maria'})
 db.people.insert({name : 'Joao'})
 db.people.insert({name : 'Joao', profession: 'pescador'})
 db.people.insert({name : 'Pedro', profession: 'Motorista'})
-db.people.insert({name : 'China', age: 30 ,  profession: 'Motorista'})
+db.people.insert({name : 'China', age: 30 ,  profession: 'Motorista'})
 db.people.insert({name : 23, profession: 'pescador'})
 db.people.insert({name : true, profession: 'pescador'})
 
-
 
-
-
-// examples of $regex $exists $type
+
+
+
+// examples of $regex $exists $type
 db.people.find({ profession: { $exists: true }})
 db.people.find({ name: { $type: 1 }})
 db.people.find({ name: { $regex: 'a$' }})
 db.people.find({ name: { $regex: 'a' }, profession: { $exists: true }})
 
 
-
-// exaples or $or
+
+// exaples or $or
 db.people.find({$or : [{name: {$regex: 'a$'}}, {age : { $exists: true }} ] } )
-db.scores.find({$or: [ { score: {$lt: 50}}, { score: { $gt: 90}} ] } )
+db.scores.find({$or: [ { score: {$lt: 50}}, { score: { $gt: 90}} ] } )
 
 // exmpale of $and
 db.people.find({$and : [{name: {$regex: 'a$'}}, {age : { $exists: true }} ] } )
 db.people.find({name: {$regex:'a$'}, age: {$exists: true}}) // short form without $and
-db.scores.find( { score : { $gt : 50 }, score : { $lt : 60 } } ); 
-
-
-
-
-// query in array
+db.scores.find( { score : { $gt : 50 }, score : { $lt : 60 } } ); 
+
+
+
+
+// query in array
 db.accounts.insert( { name: 'Robson', favorites : [ 'ice cream', 'pretzels']})
 db.accounts.insert( { name: 'Robson Duarte', favorites : [ 'Studies', 'Programmer']})
 db.accounts.insert( { name: 'Ana', favorites : [ 'Beer', 'Cana', 'Party']})
@@ -62,29 +62,29 @@ db.accounts.insert( { name: 'Tamara', favorites : [ 'Beer', 'Cana', 'Party']})
 db.accounts.insert( { name: 'Thiago', favorites : [ 'Beer', 'Beach', 'Party']})
 db.accounts.insert( { name: 'Didi', favorites : [ 'Forro', 'Beer', 'Party']})
 db.accounts.insert( { name: 'Gardenal', favorites : 'Gardneal' })
-db.accounts.insert( { name: 'Gardenal', favorites : ['Gardneal'] })
+db.accounts.insert( { name: 'Gardenal', favorites : ['Gardneal'] })
 
-db.accounts.find( {favorites: 'Gardneal'})
-
-
+db.accounts.find( {favorites: 'Gardneal'})
+
+
 // examples of $in and $all
-db.accounts.find( {favorites: { $in: [ 'Programmer', 'ice cream']}})
-
-
-
+db.accounts.find( {favorites: { $in: [ 'Programmer', 'ice cream']}})
+
+
+
 // queries with dot notation
 var obj = { name : 'Robson Duarte', email : { work : 'rduarte@r3wa.com.br', personal : 'robson.o.d@gmail.com'}}
-db.users.insert(obj)
+db.users.insert(obj)
 
 //match the document because the filter have exactly the structure of embedded email document
-db.users.find({email : { work : 'rduarte@r3wa.com.br', personal : 'robson.o.d@gmail.com'}})
+db.users.find({email : { work : 'rduarte@r3wa.com.br', personal : 'robson.o.d@gmail.com'}})
 //dont't match the document because the filter havent exactly the structure of embedded email document
 db.users.find({email : { personal : 'robson.o.d@gmail.com',work : 'rduarte@r3wa.com.br'}})
-db.users.find({email : {work : 'rduarte@r3wa.com.br'}})
-
+db.users.find({email : {work : 'rduarte@r3wa.com.br'}})
+
 // use the dot notation to access the attr of embedded document
 db.users.find({'email.work' : 'rduarte@r3wa.com.br'})
-
+
 var p1 = { product : "p1", 
   price : 9,
   reviews : [ { user : "fred", comment : "Great!" , rating : 5 },
@@ -93,26 +93,45 @@ var p1 = { product : "p1",
 var p2 = { product : "p2", 
   price : 11,
   reviews : [ { user : "fred", comment : "Great!" , rating : 6 },
-              { user : "tom" , comment : "I agree with Fred, somewhat!" , rating : 4 } ]}
-
+              { user : "tom" , comment : "I agree with Fred, somewhat!" , rating : 4 } ]}
+
 var p3 = { product : "p3", 
   price : 12,
   reviews : [ { user : "fred", comment : "Great!" , rating : 5 },
-              { user : "tom" , comment : "I agree with Fred, somewhat!" , rating : 4 } ]}
-
+              { user : "tom" , comment : "I agree with Fred, somewhat!" , rating : 4 } ]}
+
 var p4 = { product : "p4", 
   price : 13,
   reviews : [ { user : "fred", comment : "Great!" , rating : 3 },
-              { user : "tom" , comment : "I agree with Fred, somewhat!" , rating : 3 } ]}
-
+              { user : "tom" , comment : "I agree with Fred, somewhat!" , rating : 3 } ]}
+
 db.catalog.insert(p1)
 db.catalog.insert(p2)
 db.catalog.insert(p3)
 db.catalog.insert(p4)              
 
 
-db.catalog.find({price: {$gt: 10}, 'reviews.rating' : {$gte: 5}})
+db.catalog.find({price: {$gt: 10}, 'reviews.rating' : {$gte: 5}})
 
+
+// cursors
+var cur = db.people.find()
+
+cur.sort({name: -1}).limit(4).skip(2)
+
+while(cur.hasNext()){
+    print(cur.next())
+}    
+
+
+
+
+
+
+
+
+
+
 
 
 
