9dae310b6850a43f6ec634e9b69d0eff36ebd98b
Robson Duarte <robson.o.d@gmail.com>
2016-03-28 17:23:26 -0300

    add example of find with filter java driver.

---
+++ mongo-university-m101j/src/main/java/br/com/mystudies/mongo_university_m101j/FindWithFilterTest2.java
@@ -0,0 +1,83 @@
+package br.com.mystudies.mongo_university_m101j;
+
+import static com.mongodb.client.model.Filters.and;
+import static com.mongodb.client.model.Filters.eq;
+import static com.mongodb.client.model.Filters.gt;
+import static com.mongodb.client.model.Filters.lte;
+
+import java.util.ArrayList;
+
+import org.bson.Document;
+import org.bson.conversions.Bson;
+
+import com.mongodb.client.MongoCollection;
+
+public class FindWithFilterTest2 {
+
+	public static void main(String[] args) {
+		
+		
+		MongoCollection<Document> collection = new MongoConnection().getCollection("findWithFilterTest");
+		
+	
+		
+/*		IntStream.range(0, 10).forEach( i -> {
+			collection.insertOne(
+					new Document()
+						.append("x", new Random().nextInt(2))
+						.append("y", new Random().nextInt(100))
+				);
+		});*/
+		
+		
+			
+		collection.find(filter()).into(new ArrayList<>())
+			.forEach(document -> {
+				System.out.println(document);
+			});
+		
+		
+	}
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	private static Bson filter() {
+		return and(eq("x", 0), gt("y", 10), lte("y", 40));
+		
+		
+	/*	return new Document("x",0)
+					.append("y", new Document("$gt", 10))
+					.append("y", new Document("$lte", 40));*/
+	}
+	
+	
+	
+	
+}
