282575a0f84ee3f9bf9f812662b8148b0c95c9d8
Robson Duarte <robson.o.d@gmail.com>
2016-03-28 19:32:46 -0300

    add example of delete.

---
+++ mongo-university-m101j/src/main/java/br/com/mystudies/mongo_university_m101j/DeleteTest.java
@@ -0,0 +1,46 @@
+package br.com.mystudies.mongo_university_m101j;
+
+import static com.mongodb.client.model.Filters.gt;
+
+import java.util.ArrayList;
+import java.util.stream.IntStream;
+
+import org.bson.Document;
+
+import com.mongodb.client.MongoCollection;
+
+public class DeleteTest {
+
+	public static void main(String[] args) {
+		
+	
+		MongoCollection<Document> collection = new MongoConnection().getCollection("deleteTest");
+		
+
+		collection.drop();
+		
+	
+	
+		IntStream.range(0, 8).forEach( i -> {
+			collection.insertOne(new Document("_id", i));
+		});
+	
+		
+		
+//		collection.deleteMany(gt("_id", 4));
+		collection.deleteOne(gt("_id", 4));
+	
+		
+		collection.find()
+			.into(new ArrayList<>())
+			.forEach(System.out::println);
+				
+	}
+	
+	
+	
+	
+	
+	
+	
+}
