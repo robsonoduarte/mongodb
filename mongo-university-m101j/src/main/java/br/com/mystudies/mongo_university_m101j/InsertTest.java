6f61ed2b5fd2e23e841f57bbebfccc9b54a206cf
Robson Duarte <robson.o.d@gmail.com>
2016-03-28 16:43:21 -0300

    add example of insert with java driver.

--- mongo-university-m101j/src/main/java/br/com/mystudies/mongo_university_m101j/InsertTest.java
+++ mongo-university-m101j/src/main/java/br/com/mystudies/mongo_university_m101j/InsertTest.java
@@ -1,30 +1,38 @@
 package br.com.mystudies.mongo_university_m101j;
 
+import static java.util.Arrays.asList;
+
 import org.bson.Document;
 
 import com.mongodb.MongoClient;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 
-public class MongoClientApp {
+public class InsertTest {
 
 	public static void main(String[] args) {
 		
 		MongoClient mongoClient = new MongoClient("192.168.99.100", 27017);
-		
-		MongoDatabase database = mongoClient.getDatabase("test");
-		
+		MongoDatabase database = mongoClient.getDatabase("test");		
 		MongoCollection<Document> collection = database.getCollection("users");
 		
 		
-		Document document = new Document()
+		Document robson = new Document()
 					.append("name", "Robson Duarte")
-					.append("age", 39);
-		
-		System.out.println(document);
-		
+					.append("age", 39)
+					.append("profession", "programmer");
 		
 		
+		Document ana = new Document()
+				.append("name", "Ana Mara")
+				.append("age", 40)
+				.append("profession", "teacher");
+		
+		
+		
+		
+		//collection.insertOne(robson);
+		collection.insertMany(asList(robson,ana));
 		
 	}
 }
