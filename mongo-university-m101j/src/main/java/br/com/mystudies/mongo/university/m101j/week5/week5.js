2a1b0378325a7e9f37c8c5805492decc5e8b22a9
Robson Duarte <robson.o.d@gmail.com>
2016-04-13 10:26:56 -0300

    finished the Lecture simple Example expanded.

--- mongo-university-m101j/src/main/java/br/com/mystudies/mongo/university/m101j/week5/week5.js
+++ mongo-university-m101j/src/main/java/br/com/mystudies/mongo/university/m101j/week5/week5.js
@@ -23,6 +23,16 @@ db.products.aggregate([
 ])
 
 
+// simple example Expanded ( quiz )
+
+db.stuff.insert({a:1, b:1, c:1})
+db.stuff.insert({a:2, b:2, c:1})
+db.stuff.insert({a:3, b:3, c:1})
+db.stuff.insert({a:3, b:3, c:2})
+db.stuff.insert({a:3, b:5, c:3})
+
+db.stuff.aggregate([{$group:{_id: '$c'}}])
+
 db.products.aggregate([
 	{$group:
 	  	{
