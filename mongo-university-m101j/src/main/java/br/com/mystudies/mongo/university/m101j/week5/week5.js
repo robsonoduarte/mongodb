df28d9c077c244a7b2485f2e9f86d1ba3c4eed48
Robson Duarte <robson.o.d@gmail.com>
2016-04-16 12:07:46 -0300

    finished the lecture double $unwind.

--- mongo-university-m101j/src/main/java/br/com/mystudies/mongo/university/m101j/week5/week5.js
+++ mongo-university-m101j/src/main/java/br/com/mystudies/mongo/university/m101j/week5/week5.js
@@ -406,5 +406,46 @@ db.posts.aggregate([
 
 
 
+// double $unwind
+
+db.inventory.aggregate([
+	{$unwind:'$sizes'},
+	{$unwind:'$colors'},
+	{$group:
+	  {
+	    _id:{size:'$sizes', color:'$colors'},
+	    count:{$sum:1}
+	  }
+	}
+])
+
+
+// reverse double unwind
+
+db.inventory.aggregate([
+	{$unwind:'$sizes'},
+	{$unwind:'$colors'},
+	{$group:
+	  {
+	    _id:{name:'$name', size:'$sizes'},
+	    colors:{$push:'$colors'}
+	  }
+	},
+	{$group:
+	  	{
+	  	  _id:{name:'$_id.name', colors: '$colors'},
+		   sizes:{$push: '$_id.size'}
+		}
+	},
+	{$project:
+	  {
+	    _id:0,
+	    name:'$_id.name',
+	    sizes:1,
+	    colors: '$_id.colors'
+	  }
+	}
+])
+
 
 
