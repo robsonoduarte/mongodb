65d2ed17ed84343a7d84f67040562f02b1dbc1ba
Robson Duarte <robson.o.d@gmail.com>
2016-04-16 11:31:09 -0300

    finished the lecture using $limit and $skip

--- mongo-university-m101j/src/main/java/br/com/mystudies/mongo/university/m101j/week5/week5.js
+++ mongo-university-m101j/src/main/java/br/com/mystudies/mongo/university/m101j/week5/week5.js
@@ -352,7 +352,34 @@ db.zips.aggregate([
 ])
 
 
+// revisiting $first and $last
 
+db.zips.aggregate([
+	{$group:
+	  	{
+	  	  _id:{state:'$state', city:'$city'},
+	  	  pop:{$sum:'$pop'}
+	  	 }
+	 },
+	 {$sort:
+	   {
+	     '_id.state':1,
+	     pop:-1
+	   }
+	  },
+	  {$group:
+	    {
+	      _id:'$_id.state',
+	      city:{$first:'$_id.city'},
+	      pop:{$first:'$pop'}
+	    }
+	  },
+	  {$sort:
+	    {
+	      _id:1
+	    }
+	  }
+])
 
 
 
