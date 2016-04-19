1edb9d114b44885f6f885cdffce1830808cb044e
Robson Duarte <robson.o.d@gmail.com>
2016-04-19 08:59:16 -0300

    finished homework 5.1

--- mongo-university-m101j/src/main/java/br/com/mystudies/mongo/university/m101j/week5/week5.js
+++ mongo-university-m101j/src/main/java/br/com/mystudies/mongo/university/m101j/week5/week5.js
@@ -448,4 +448,19 @@ db.inventory.aggregate([
 ])
 
 
+// homework 5.1
+
+
+db.small_posts.aggregate([
+	{$unwind:'$comments'},
+	{$group:
+	  {
+	    _id: '$comments.author',
+	    count:{$sum:1}
+	   }
+	 },
+	 {$sort:{count:-1}}
+])
+
+
 
