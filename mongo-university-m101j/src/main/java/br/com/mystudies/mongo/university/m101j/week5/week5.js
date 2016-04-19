12936c5c192a5f41a7a39a3458ded3714e51301e
Robson Duarte <robson.o.d@gmail.com>
2016-04-19 10:13:37 -0300

    finished homework 5.3

--- mongo-university-m101j/src/main/java/br/com/mystudies/mongo/university/m101j/week5/week5.js
+++ mongo-university-m101j/src/main/java/br/com/mystudies/mongo/university/m101j/week5/week5.js
@@ -486,3 +486,23 @@ db.small_zips.aggregate([
 ])
 
 
+// homework 5.3
+
+db.grades.aggregate([
+		{$unwind:'$scores'},
+		{$match:{'scores.type':{$in:['exam', 'homework']}}},
+		{$group:
+		  {
+		    _id:{student_id:'$student_id', class_id:'$class_id'},
+		   avg:{$avg:'$scores.score'}
+		  }
+		},
+		{$group:
+		  {
+		  	_id:'$_id.class_id',
+		  	avg:{$avg:'$avg'}
+		  }
+		},
+		{$sort:{avg:-1}}
+	])
+
