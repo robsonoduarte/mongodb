59eeba0209d52d2da00e625e5a5b0501a3495fc6
Robson Duarte <robson.o.d@gmail.com>
2016-04-21 15:01:16 -0300

    fix the port 27019

--- mongo-university-m101j/src/main/java/br/com/mystudies/mongo/university/m101j/week6/start.sh
+++ mongo-university-m101j/src/main/java/br/com/mystudies/mongo/university/m101j/week6/start.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-
 mongod --port 27017 --dbpath /data/rs1 --replSet m101 &
 mongod --port 27018 --dbpath /data/rs2 --replSet m101 &
-mongod --port 27029 --dbpath /data/rs3 --replSet m101
+mongod --port 27019 --dbpath /data/rs3 --replSet m101
 
