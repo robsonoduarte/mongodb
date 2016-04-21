840e4826a3dba7a062b827c4902658f2ba35013f
Robson Duarte <robson.o.d@gmail.com>
2016-04-21 15:01:53 -0300

    add js to config replica set.

--- mongo-university-m101j/src/main/java/br/com/mystudies/mongo/university/m101j/week6/week6.js
+++ mongo-university-m101j/src/main/java/br/com/mystudies/mongo/university/m101j/week6/week6.js
@@ -1,3 +1,25 @@
-/**
- *
- */
+config = {
+
+	_id: 'm101',
+	members:[
+		{
+		  _id:0,
+		  host:'07ac44186bea:27017',
+		  priority:0,
+		  slaveDelay:5
+		},
+		{
+		  _id:1,
+		  host:'07ac44186bea:27018'
+		},
+		{
+		  _id:2,
+		  host:'07ac44186bea:27019'
+		}
+	]
+}
+
+
+rs.initiate(config)
+rs.status()
+
