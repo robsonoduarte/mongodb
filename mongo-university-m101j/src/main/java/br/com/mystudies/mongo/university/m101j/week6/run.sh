48ff6451b1351c7a9eb6f40efa5937ce8cb4d8ab
Robson Duarte <robson.o.d@gmail.com>
2016-04-21 15:00:39 -0300

    remove bash o command and add -d

--- mongo-university-m101j/src/main/java/br/com/mystudies/mongo/university/m101j/week6/run.sh
+++ mongo-university-m101j/src/main/java/br/com/mystudies/mongo/university/m101j/week6/run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run -d --name mongo-replica-set -p 27017:27017 -p 27018:27018 -p 27019:27019  mongo-replica-set bash
+docker run -d --name mongo-replica-set -p 27017:27017 -p 27018:27018 -p 27019:27019  mongo-replica-set
