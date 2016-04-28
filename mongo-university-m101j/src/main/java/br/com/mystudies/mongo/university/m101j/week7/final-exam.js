2844c7c4fd2cbe6183c1aaf2a983b9ec56ca436a
Robson Duarte <robson.o.d@gmail.com>
2016-04-28 10:27:31 -0300

    done question 1

--- mongo-university-m101j/src/main/java/br/com/mystudies/mongo/university/m101j/week7/final-exam.js
+++ mongo-university-m101j/src/main/java/br/com/mystudies/mongo/university/m101j/week7/final-exam.js
@@ -1,1 +1,7 @@
 // question 1
+// query for reference
+db.messages.find({$and:[ {'headers.From' : 'andrew.fastow@enron.com'}, {'headers.To' : 'john.lavorato@enron.com'}]}).count()
+
+// the query of question
+db.messages.find({$and:[ {'headers.From' : 'andrew.fastow@enron.com'}, {'headers.To' : 'jeff.skilling@enron.com'}]}).count()
+
