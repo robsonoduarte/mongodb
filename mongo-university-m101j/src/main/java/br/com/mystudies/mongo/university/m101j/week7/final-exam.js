ee4e4e69355c26c860bd04531835304e89fb2b4d
Robson Duarte <robson.o.d@gmail.com>
2016-04-28 11:22:02 -0300

    done question 2.

--- mongo-university-m101j/src/main/java/br/com/mystudies/mongo/university/m101j/week7/final-exam.js
+++ mongo-university-m101j/src/main/java/br/com/mystudies/mongo/university/m101j/week7/final-exam.js
@@ -1,7 +1,35 @@
-// question 1
+// QUESTION 1
 // query for reference
 db.messages.find({$and:[ {'headers.From' : 'andrew.fastow@enron.com'}, {'headers.To' : 'john.lavorato@enron.com'}]}).count()
-
 // the query of question
 db.messages.find({$and:[ {'headers.From' : 'andrew.fastow@enron.com'}, {'headers.To' : 'jeff.skilling@enron.com'}]}).count()
+
+
+
+
+
+
+// QUESTION 2
+db.messages.aggregate([
+	{$unwind: '$headers.To'},
+	{$group:
+	  {
+	    _id:{id: '$_id', from: '$headers.From', to: '$headers.To'},
+	    to:{$addToSet:'$headers.To'}
+	  }
+	},
+	{
+	   $unwind:'$to'
+ 	},
+	{$group:
+	  {
+	    _id:{from: '$_id.from', to: '$_id.to'},
+	    count:{$sum:1}
+	  }
+	},
+	{$sort:{count:-1}},
+	{$limit:10}
+],
+{allowDiskUse: true}
+)
 
