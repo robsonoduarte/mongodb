e15e5180830fc0ade94f71eb0fb664ab11c7e689
Robson Duarte <robson.o.d@gmail.com>
2016-04-30 10:10:14 -0300

    done question 3.

--- mongo-university-m101j/src/main/java/br/com/mystudies/mongo/university/m101j/week7/final-exam.js
+++ mongo-university-m101j/src/main/java/br/com/mystudies/mongo/university/m101j/week7/final-exam.js
@@ -33,3 +33,11 @@ db.messages.aggregate([
 {allowDiskUse: true}
 )
 
+
+
+
+// QUESTION 3
+
+db.messages.find({'headers.Message-ID': '<8147308.1075851042335.JavaMail.evans@thyme>'})
+db.messages.update({'headers.Message-ID': '<8147308.1075851042335.JavaMail.evans@thyme>'}, {$addToSet:{'headers.To': 'mrpotatohead@mongodb.com'}})
+
